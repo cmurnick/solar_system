19b186f4df9e06def5d38cadbe3ab9f7a92349e4
Caitlin Murnick <caitlinmurnick@gmail.com>
2017-09-12 21:26:02 -0500

    setup all files

---
+++ javascripts/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const solarSystem = require('./solarSystem');
+
+// document.getElementById('button').addEventListener('click', (event) => {
+// 	let languageSelected = event.target.id;
+// 	if (event.target.id ==='button') {
+// 		languageSelected = 'english';
+// 	} else {
+// 		languageSelected = event.target.id;
+
+// 	}
+// 	console.log(languageSelected);
+// 	document.getElementById("output").innerHTML = GreetingGenerator[languageSelected]();
+// });
---
+++ javascripts/solarSystem.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const mercury = require('./mercury.js');
+const venus = require('./venus.js');
+const earth = require('./earth.js');
+const mars = require('./mars.js');
+const jupiter = require('./jupiter.js');
+const saturn = require('./saturn.js');
+const uranus = require('./uranus.js');
+const neptune = require('./neptune.js');
+
+
+
+const solarSystem = {
+
+	mercury, venus, earth, mars, jupiter, saturn, neptune
+
+};
